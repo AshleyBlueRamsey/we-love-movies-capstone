57fcaa6086694d008a1c4ecbd32467e4bc054093
Ashley Ramsey <dusty1boy1984@gmail.com>
2023-02-21 07:58:32 -0500

    updated exports.down

--- src/db/migrations/20230220202239_movieTheaters.js
+++ src/db/migrations/20230220202239_movieTheaters.js
@@ -20,5 +20,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("movies_theaters");
+  return knex.schema.dropTableIfExists("movies_theaters");
 };
